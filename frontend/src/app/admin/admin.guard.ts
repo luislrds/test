9b53e5f623dcf09fde1abce789957c889f7cb078
luislrds <luislrds@gmail.com>
2020-05-01 11:46:31 -0500

    Proyecto prueba

---
+++ frontend/src/app/admin/admin.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuard implements CanActivate {
+  constructor( private router : Router)
+  {}
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if(localStorage.getItem("ACCESS_TOKEN")){
+      return true
+    }else{
+      return this.router.parseUrl("auth/login")
+    }
+  }
+  
+}
